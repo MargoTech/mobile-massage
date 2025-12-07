be349be54f747d30ac7198eb0267b74498f71cce
MargoTech <margobartashevich@gmail.com>
2025-12-07 12:00:00 +0100

    Adding therapist.js

--- src/pages/Therapists.jsx
+++
@@ -1,5 +0,0 @@
-const Therapists = () => {
-  return <h1 className="text-2xl">Our Therapists</h1>;
-};
-
-export default Therapists;
---
+++ src/types/therapists.js
@@ -0,0 +1,18 @@
+export const therapists = [
+  {
+    id: "t1",
+    name: "Margarita Bartashevich",
+    specialization: ["Medical massage"],
+    pricePerOur: 80,
+    rating: 5.0,
+    isAvailable: true,
+  },
+  {
+    id: "t2",
+    name: "Maria Petrova",
+    specialization: ["Relaxing"],
+    pricePerHour: 60,
+    rating: 4.9,
+    isAvailable: false,
+  },
+];
